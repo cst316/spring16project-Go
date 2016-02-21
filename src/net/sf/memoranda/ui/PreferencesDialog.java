a3c032c1f6894b0e34295b0f9762f236892cc9c7
BinHong Lee <binhong@binhong.me>
2016-02-20 20:53:11 -0700

    Start minimized & On minimized
    Start minimized
    Start minimized will now work properly as the program is immediately
    minimized right after it is started. The option in the Preference Dialog
    Box is also re-enabled.
    
    On minimized
    The program now reads from the configuration file to decide if to "hide"
    or to "minimize to taskbar". It will also write to the configuration
    file according to the user's option in the preference dialog.

--- src/net/sf/memoranda/ui/PreferencesDialog.java
+++ src/net/sf/memoranda/ui/PreferencesDialog.java
@@ -368,7 +368,6 @@ public class PreferencesDialog extends JDialog {
 		gbc.anchor = GridBagConstraints.WEST;
 		GeneralPanel.add(enSystrayChB, gbc);
 		startMinimizedChB.setText(Local.getString("Start minimized"));
-		startMinimizedChB.setEnabled(false);
 		
 		gbc = new GridBagConstraints();
 		gbc.gridx = 1;
@@ -573,7 +572,11 @@ public class PreferencesDialog extends JDialog {
 		}
 
 		String onmin = Configuration.get("ON_MINIMIZE").toString();
-		this.minTaskbarRB.setSelected(true);
+		if (onmin.equals("normal")) {
+			this.minTaskbarRB.setSelected(true);
+		} else {
+			this.minHideRB.setSelected(true);
+		}
 
 		if (!System.getProperty("os.name").startsWith("Win"))
 			this.browserPath.setText(MimeTypesList.getAppList()
@@ -660,7 +663,10 @@ public class PreferencesDialog extends JDialog {
 		else
 			Configuration.put("ON_CLOSE", "minimize");
 
-		Configuration.put("ON_MINIMIZE", "normal");
+		if (this.minTaskbarRB.isSelected())
+			Configuration.put("ON_MINIMIZE", "normal");
+		else
+			Configuration.put("ON_MINIMIZE", "hide");
 
 		String lf = Configuration.get("LOOK_AND_FEEL").toString();
 		String newlf = "";
