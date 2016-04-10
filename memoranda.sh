db95f9bd7b34efa3b5ba408aceb700cd9dd0beef
aacalde1 <aacalde1@asu.edu>
2016-04-09 20:29:10 -0700

    comment

--- memoranda.sh
+++ memoranda.sh
@@ -5,3 +5,4 @@ export MEMORANDA_HOME=./data
 LCP="./build/memoranda.jar:./lib/xom-1.0.jar:./lib/xercesImpl.jar:./lib/xmlParserAPIs.jar:./lib/nekohtml.jar:./lib/nekohtmlXni.jar"
 
 java -cp ${LCP} net.sf.memoranda.Start $1
+#
